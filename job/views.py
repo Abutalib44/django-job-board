652a78e1972d6878aa198014cb8b2b81cec9c601
ahmed <ahmedabutalib44@gmail.com>
2021-03-13 23:13:09 +0200

    Now job detail by ID work

--- job/views.py
+++ job/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import job
 # Create your views here.
+
 def job_list(request):
     job_list= job.objects.all()
     context={'jobs':job_list}
@@ -8,6 +9,6 @@ def job_list(request):
 
 
 def job_details(request,id):
-    job.objects.get(id=id)
-    context={'job': job_details}
+    job_detail=job.objects.get(id=id)
+    context={'job': job_detail}
     return render(request,'job/job_details.html',context)
