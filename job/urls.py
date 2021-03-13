9f5d94bfd3880020db3c11daf25c73182cdadef8
ahmed <ahmedabutalib44@gmail.com>
2021-03-13 03:49:42 +0200

    adding job details

--- job/urls.py
+++ job/urls.py
@@ -2,6 +2,6 @@ from django.urls import path ,include
 from . import views
 
 urlpatterns = [
-    path('',views.job_list)
-    path('<int:id>',views.job_details)
+    path('',views.job_list),
+    path('<int:id>',views.job_details),
 ]
